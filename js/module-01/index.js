ecff9cf1dd4af449ae33007d1d7d3cafe1daae22
Stacy <enjoyorhate@gmail.com>
2019-02-18 08:33:48 +0200

    added new folder and module-01

---
+++ js/module-01/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+
+let login, password;
+
+login = prompt ('Введите логин');
+
+if (login === null) {
+    alert('Отменено пользователем!');
+}
+
+else if (login !== adminLogin) {
+    alert('Доступ запрещен, неверный логин!');
+}
+
+else if (login === adminLogin) {
+    password = prompt ('Введите пароль');
+}
+
+if (password === null) {
+    alert('Отменено пользователем!');
+}
+
+else if (password !== adminPassword) {
+    alert('Доступ запрещен, неверный пароль!');
+}
+
+else if (password === adminPassword) {
+    alert('Добро пожаловать!');
+}
