6bd2f90954007e51fa7c391d35016197a123bb36
Stacy <enjoyorhate@gmail.com>
2019-02-27 00:01:02 +0200

    added isNaN check; added an additional task

---
+++ js/module-02/additional/additional.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+let attempts = 3,
+    userPass,
+    n = attempts;
+
+do {
+  userPass = prompt('Введите пароль');
+  
+  if (passwords.includes(userPass)) {
+
+    alert('Добро пожаловать!');
+
+    break;
+  }
+  
+  if (userPass === null) {
+    
+    break;
+    
+  }
+
+  if (!passwords.includes(userPass)) {
+    
+    n -= 1;
+
+    alert(`Неверный пароль, у вас осталось ${n} попыток`);
+  }
+
+  console.log(n);
+
+} while ( n > 0 );
+
+if (n === 0) {
+
+  alert('У вас закончились попытки, аккаунт заблокирован!');
+
+}
--- js/module-02/index.js
+++ js/module-02/index.js
@@ -1,39 +1,43 @@
-'use strict'
+"use strict";
 
 let userInput;
 const numbers = [];
 let total = 0;
 
-do { 
-    userInput = prompt('Введите любое число');
+do {
+  userInput = prompt("Введите любое число");
 
-    numbers.push(userInput)
+  if (Number(userInput)) {
 
-    console.log(numbers);
+  numbers.push(userInput);
 
-    if (!Number.isInteger(Number(userInput))) {
-        alert('Было введено не число, попробуйте еще раз');
+  }
+  
+  if (isNaN(userInput)) {
 
-        numbers.pop();
-    }
+    alert("Было введено не число, попробуйте еще раз");
+
+  }
+
+
+} while (userInput !== null);
+
+  console.log(numbers);
+
+for (const item of numbers) {
+  let itemNum = Number(item);
+
+  total += itemNum;
 }
 
-while (userInput !== null);
-
-for ( const item of numbers) {
-
-    total = total + Number(item);
-    
-    console.log(total);
-    
-}
+console.log(total);
 
 alert(`Общая сумма чисел равна ${total}`);
 
 // for (let i = 0; i < numbers.length; i += 1) {
 
-//     total = total + Number(numbers[i]);
-    
+//     total += Number(numbers[i]);
+
 //     console.log(total);
 
 // }
