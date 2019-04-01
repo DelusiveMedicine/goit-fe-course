0005de00d8e6d9c1d04dd91be6e607dfb7837343
Stacy <enjoyorhate@gmail.com>
2019-04-01 09:06:38 +0300

    added additional task

--- js/module-05/index.js
+++ js/module-05/index.js
@@ -179,3 +179,28 @@ const getUsersByFriend = (arr, name) => {
 console.log(getUsersByFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersByFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
                                             
+/**
+* Получить массив всех скиллов всех пользователей (поле skills), при этом не должно быть
+* повторяющихся скиллов и они должны быть отсортированы в алфавитном порядке
+*/
+const getAllUniqueSkills = arr => {
+  return arr.reduce((acc, user) => acc.concat(user.skills), []);
+};
+
+const getAllSkills = arr => {
+  return getAllUniqueSkills(arr).filter((skill, i, set) => set.indexOf(skill) === i).sort();
+}
+
+console.log(getAllSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+                             
+/**
+* Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+*/
+const getUserNamesSortedByFriendsCount = arr => {
+  return arr.sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
+};
+
+console.log(getUserNamesSortedByFriendsCount(users)); 
+[ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
