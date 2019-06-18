7eb298de3972d1c13949a7251358bbe68fd2e2e1
Stacy <enjoyorhate@gmail.com>
2019-06-18 08:09:50 +0200

    build

--- js/module-12/build/js/scripts.js
+++ js/module-12/build/js/scripts.js
@@ -6787,7 +6787,7 @@ function validateBookmark(newInput) {
 
 function lookUpBookmark(url) {
   var apiKey = '5d07709a7e4b03cf4f38d85d743385432afe95e3e2cc5';
-  fetch('http://api.linkpreview.net/?key=' + apiKey + '&q=' + url, {
+  fetch('https://api.linkpreview.net/?key=' + apiKey + '&q=' + url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
