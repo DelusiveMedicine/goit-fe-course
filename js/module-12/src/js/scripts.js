fd00e6907c4dd97c00e5eab025602b4e762318f4
Stacy <enjoyorhate@gmail.com>
2019-06-18 07:59:11 +0200

    fixed url

--- js/module-12/src/js/scripts.js
+++ js/module-12/src/js/scripts.js
@@ -36,7 +36,7 @@ function validateBookmark(newInput) {
 function lookUpBookmark(url) {
   const apiKey = '5d07709a7e4b03cf4f38d85d743385432afe95e3e2cc5';
 
-  fetch('http://api.linkpreview.net/?key=' + apiKey + '&q=' + url, {
+  fetch('https://api.linkpreview.net/?key=' + apiKey + '&q=' + url, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   })
